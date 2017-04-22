f6ebcfcbddd10c116c2113900677997bc1b9effc
shimba <e34637@jp.ibm.com>
2017-04-22 10:52:38 +0900

    fine tuning

---
+++ fine-tuning/fine-tuning.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 23:45:49 2017
+
+@author: http://aidiary.hatenablog.com/entry/20170104/1483535144
+"""
+
+import os
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, Model
+from keras.layers import Input, Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+import numpy as np
+#from smallcnn import save_history
+
+def save_history(history, result_file):
+    loss = history.history['loss']
+    acc = history.history['acc']
+    val_loss = history.history['val_loss']
+    val_acc = history.history['val_acc']
+    nb_epoch = len(acc)
+
+    with open(result_file, "w") as fp:
+        fp.write("epoch\tloss\tacc\tval_loss\tval_acc\n")
+        for i in range(nb_epoch):
+            fp.write("%d\t%f\t%f\t%f\t%f\n" % (i, loss[i], acc[i], val_loss[i], val_acc[i]))
+
+classes = ['Tulip', 'Snowdrop', 'LilyValley', 'Bluebell', 'Crocus',
+           'Iris', 'Tigerlily', 'Daffodil', 'Fritillary', 'Sunflower',
+           'Daisy', 'ColtsFoot', 'Dandelion', 'Cowslip', 'Buttercup',
+           'Windflower', 'Pansy']
+
+batch_size = 32
+nb_classes = len(classes)
+
+img_rows, img_cols = 150, 150
+channels = 3
+
+train_data_dir = 'dataset/train_images'
+validation_data_dir = 'dataset/test_images'
+
+nb_train_samples = 1190
+nb_val_samples = 170
+nb_epoch = 50
+
+result_dir = 'dataset/results'
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+
+
+if __name__ == '__main__':
+    # VGG16モデルと学習済み重みをロード
+    # Fully-connected層（FC）はいらないのでinclude_top=False）
+    input_tensor = Input(shape=(img_rows, img_cols, 3))
+    vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    # vgg16.summary()
+
+    # FC層を構築
+    # Flattenへの入力指定はバッチ数を除く
+    top_model = Sequential()
+    top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
+    top_model.add(Dense(256, activation='relu'))
+    top_model.add(Dropout(0.5))
+    top_model.add(Dense(nb_classes, activation='softmax'))
+
+    # 学習済みのFC層の重みをロード
+    # top_model.load_weights(os.path.join(result_dir, 'bottleneck_fc_model.h5'))
+
+    # VGG16とFCを接続
+    model = Model(input=vgg16.input, output=top_model(vgg16.output))
+
+    # 最後のconv層の直前までの層をfreeze
+    for layer in model.layers[:15]:
+        layer.trainable = False
+
+    # Fine-tuningのときはSGDの方がよい？
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  metrics=['accuracy'])
+
+    train_datagen = ImageDataGenerator(
+        rescale=1.0 / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        color_mode='rgb',
+        classes=classes,
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=True)
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        color_mode='rgb',
+        classes=classes,
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=True)
+
+    # Fine-tuning
+    history = model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        nb_epoch=nb_epoch,
+        validation_data=validation_generator,
+        nb_val_samples=nb_val_samples)
+
+    model.save_weights(os.path.join(result_dir, 'finetuning.h5'))
+    save_history(history, os.path.join(result_dir, 'history_finetuning.txt'))
